734dd7212bf5592a2d566d547457583e5fa26b2a
uscracken <colfarrell@yandex.ru>
2021-04-22 12:22:59 +0300

    ready everything

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 import './style.sass';
+
+const btn = document.querySelector(".nav_btn");
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    btn.parentNode.parentNode.classList.toggle('closed_dropdown');
+})
+
+const str_danger = document.getElementById("nav_btn5");
+let width = window.innerWidth;
+let cut, res;
+if(width <= 1200) {
+    cut = 20;
+    res = str_danger.textContent.slice(0, cut);
+} else {
+    if(width > 1200) {
+        cut = 22;
+        res = str_danger.textContent.slice(0, cut);
+    }
+}
+
+str_danger.innerHTML = `${res}`;
